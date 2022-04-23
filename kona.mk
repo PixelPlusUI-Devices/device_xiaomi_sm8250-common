895d91f03ec6f5b8a76333668937ea24f2774169
johnmart19 <johnivan19999@gmail.com>
2022-04-23 21:55:38 +0300

    sm8250-common: Update android.software.vulkan.deqp.level, version 2021-03-01 - Updated Blobs having 2021 released drivers

--- kona.mk
+++ kona.mk
@@ -76,7 +76,7 @@ PRODUCT_COPY_FILES += \
 
 ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 endif
 
 # A/B
